bc29562da7d2620ebdb08e6840b49a5601bef49e
Parrot <dungpro909@gmail.com>
2025-10-03 19:24:46 +0700

    chỉnh lại html

--- bk.py
+++ bk.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, session, redirect, url_for, request, redirect, url_for, request
 import random
 
-app = Flask(__name__)
-app.secret_key = 'Parrot'
+bk = Flask(__name__)
+bk.secret_key = 'Parrot'
 
 def start():
-    sessionp['coins'] = 500
-    sessionp['message'] = 'Chào mừng đến với địa ngục'
+    session['coins'] = 500
+    session['message'] = 'Chào mừng đến với địa ngục'
 
-@app.route('/')
+@bk.route('/')
 def index():
     if 'coins' not in session:
         start()
     coins = session['coins']
     message = session.get('message', '')
     if coins <= 0:
-        return render_template('kick.html')
+        return render_template('lose.html')
     if coins >= 20000:
         return render_template('win.html', coins = coins)
-    return render_template('blackjack.html', coins=coins, message=message)
+    return render_template('bj.html', coins=coins, message=message)
     
-@app.route('/play', methods=['POST'])
+@bk.route('/play', methods=['POST'])
 def play():
     try:
         bet = int(request.form.get('bet', 200))
@@ -43,10 +43,10 @@ def play():
         session['message'] = "Dme cược lại ngay!"
     return redirect(url_for('index'))
 
-@app.route('/restart')
+@bk.route('/restart')
 def restart():
     start()
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    bk.run(debug=True)
